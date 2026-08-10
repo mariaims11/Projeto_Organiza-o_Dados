0e45c27f8d11304ec8acb9a92b14939d9184317e
mariaims11 <marimims2002@gmail.com>
2026-08-10 09:09:11 +0100

    Update hora1_teste.sql

--- hora1_teste.sql
+++ hora1_teste.sql
@@ -6,7 +6,7 @@ SELECT 'Ambiente configurado com sucesso!' AS estado;
 +-----------------------+         +-----------------------+         +-----------------------+
 
 |                       |         |                       |         |                       |
-|     UTILIZADORES      |         |   VENDAS/TRANSAÇÕES   |         |       PRODUTOS        |
+|     UTILIZADORES      |         |   VENDAS TRANSAÇÕES   |         |       PRODUTOS        |
 |                       |         |                       |         |                       |
 | (Quem compra / faz a  |-------->| (Registo de quem      |-------->| (Os livros /          |
 |      formação)        |         |   comprou o quê)      |         |     os cursos)        |
