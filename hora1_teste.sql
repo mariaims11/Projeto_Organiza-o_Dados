4d9fdddcd9e8377df603ee4f9335bb9ddbd19866
mariaims11 <marimims2002@gmail.com>
2026-08-10 09:08:58 +0100

    Update hora1_teste.sql

--- hora1_teste.sql
+++ hora1_teste.sql
@@ -1,3 +1,13 @@
 -- Formando: Maria Soares
 -- Teste de Ligacao PostgreSQL Cloud
 SELECT 'Ambiente configurado com sucesso!' AS estado;
+
+
++-----------------------+         +-----------------------+         +-----------------------+
+
+|                       |         |                       |         |                       |
+|     UTILIZADORES      |         |   VENDAS/TRANSAÇÕES   |         |       PRODUTOS        |
+|                       |         |                       |         |                       |
+| (Quem compra / faz a  |-------->| (Registo de quem      |-------->| (Os livros /          |
+|      formação)        |         |   comprou o quê)      |         |     os cursos)        |
++-----------------------+         +-----------------------+         +-----------------------+
